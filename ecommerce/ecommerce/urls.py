775d65ddfac8e63eb8636204871ef687955f39e5
gmarcusmartinez <gmarcusmartinez@gmail.com>
2024-10-11 22:08:05 +0200

    setup drf_spectacular and swagger

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from ecommerce.product import views
 
@@ -9,5 +10,7 @@ router.register("category", views.CategoryViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(router.urls))
+    path("api/", include(router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema"))
 ]
