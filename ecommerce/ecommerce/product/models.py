ae4e1a3e779c57aadf49cd65fe04d183e1eaf81f
gmarcusmartinez <gmarcusmartinez@gmail.com>
2024-10-13 12:01:25 +0200

    modify admin and relate fields

--- ecommerce/ecommerce/product/models.py
+++ ecommerce/ecommerce/product/models.py
@@ -29,14 +29,14 @@ class Brand(models.Model):
 
 
 class Product(models.Model):
-    is_active = models.BooleanField(default=False)
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    is_digital = models.BooleanField(default=False)
-    slug = models.SlugField(max_length=255, unique=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey('Category', null=True,
                               blank=True, on_delete=models.SET_NULL)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=False)
+    is_digital = models.BooleanField(default=False)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
 
     objects = ActiveQuerySet.as_manager()
 
@@ -47,17 +47,43 @@ class Product(models.Model):
 class ProductLine(models.Model):
     is_active = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    sku = models.CharField(max_length=255)
-    stock_qty = models.IntegerField()
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="product_line")
     sequence = OrderField(unique_for_field="product", blank=True)
+    sku = models.CharField(max_length=255)
+    stock_qty = models.IntegerField()
 
     def clean(self):
-        qs = ProductLine.objects.filter(product=self.product)
+        qs = ProductLine.objects.filter(product=self.product_id)
         for obj in qs:
             if self.id != obj.id and self.sequence == obj.sequence:
-                raise ValidationError("Sequence must be unique per product")
+                raise ValidationError("Duplicate value.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ProductLine, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.sku}"
+        return self.sku
+
+
+class ProductImage(models.Model):
+    alt_text = models.CharField(max_length=255)
+    product_line = models.ForeignKey(
+        ProductLine, on_delete=models.CASCADE, related_name="product_image")
+    sequence = OrderField(unique_for_field="product_line", blank=True)
+    url = models.ImageField(upload_to=None, default="test.jpg")
+
+    def clean(self):
+        qs = ProductImage.objects.filter(product_line=self.product_line)
+        for obj in qs:
+            if self.id != obj.id and self.sequence == obj.sequence:
+                raise ValidationError(
+                    "Sequence must be unique per product line")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ProductImage, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.url)
